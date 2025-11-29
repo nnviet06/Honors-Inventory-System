74ccca080098755c8678f4c06912e9f0075d66fa
nnviet06 <nnviet2016@gmail.com>
2025-11-28 23:53:05 -0500

    Add Search Bar

--- frontend/src/components/layout/NavBar.tsx
+++ frontend/src/components/layout/NavBar.tsx
@@ -1,10 +1,13 @@
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
+    // Simple NavBar
+    // The Main Page is already on By Items view
+    // Will work on "By Location" button later if I have time. 
 
     return (
         <nav>
-            <h1 className={styles.title}> 📦 WELCOME TO THE HONORS INVENTORY SYSTEM</h1>
+            <h1 className={styles.title}>📦 WELCOME TO THE HONORS INVENTORY SYSTEM</h1>
             <div className={styles.navButtons}>
             <button className={styles.navButton}> By Items  </button>
             <button className={styles.navButton}> By Location </button>
--- frontend/src/components/layout/SearchBar.tsx
+++ frontend/src/components/layout/SearchBar.tsx
@@ -1,14 +1,36 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import styles from './SearchBar.module.css';
 
-const SearchBar = () => {       
+const SearchBar = () => {    
+    // Two main components: Search button and Filter button
     const [query, setQuery] = useState<string>('');
+    const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
+
+    const toggleFilter = () => setIsFilterOpen(!isFilterOpen);
+
     return (
-        <div>
-            <p> Search Bar Component </p>  
-            <p> Query: {query}  </p>
+        <div className={styles.searchBar}>  
+            <input
+                className={styles.searchInput}
+                type="text" 
+                placeholder=" Search for items..." 
+                value={query} 
+                onChange={(e) => setQuery(e.target.value)} />    {/* Setting up userState for Search button component*/}
+            <button className={styles.searchButton}> Search </button>  
+
+            <div className={styles.filterContainer}>
+            <button className={styles.filterButton} onClick={toggleFilter}> 
+                Filter 
+            </button>
+            {isFilterOpen && (
+                <div className={styles.filterDropdown}>
+                    <p>Filter options</p>   {/* Setting up userState for Filter button component*/}
+                </div>
+            )}
+        </div>
         </div>
     );
+    
 }
 
 export default SearchBar;
