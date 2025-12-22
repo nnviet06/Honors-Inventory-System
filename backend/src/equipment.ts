2e3c1808f7a8c36350acb8a50596c0f810847611
nnviet06 <nnviet2016@gmail.com>
2025-12-22 00:21:13 -0500

    Define interfaces for backend, partially done

---
+++ backend/src/equipment.ts
@@ -0,0 +1,8 @@
+// Define types, interfaces related to equipment management in the frontend
+
+export interface Location {
+  id: number;
+  room_name: string;
+  building_type: string;
+  created_at: Date;
+}
